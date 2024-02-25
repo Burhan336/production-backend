534e52e2fc17038005086579e0e850792733ea53
Burhan Saqib <burhan.sq4906@gmail.com>
2024-02-25 20:08:14 +0500

    utils discussion | custom api response and handling

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "/env" });
 
-connectDB();
+connectDB()
+  .then(
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log("DB connection failed!", err);
+  });
 
 // import express from "express";
 
